879d68de2a22e9fd792c5e6095205d8b8d3a9199
Zaynne Silva <zaysilva01@gmail.com5~2~2~git>
2021-02-14 15:24:06 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var nome = "Zaynne Silva";
+var cargo = "CTO - Growdev";
+
+var nomehtml = document.getElementById ("nome-no-html");
+var cargohtml = document.getElementById ("cargo-no-html");
+var text1 = document.getElementById ("text-1");
+var text2 = document.getElementById ("text-2");
+
+function colocarNomeNoHtml (nome){
+nomehtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml (cargo) {
+    cargohtml.innerHTML = cargo;
+}
+
+function logarNome(nome) {
+    console.log (nome);
+}
+
+function clickNoProjetos() {
+    console.log ("clicou no botão Projetos");
+    text2.style.display = "block";
+    text1.style.display = "none";
+}
+
+function clickNoSobre() {
+    console.log ("clicou no botão Sobre");
+    text1.style.display = "block";
+    text2.style.display = "none";
+}
+
+colocarNomeNoHtml (nome);
+colocarCargoNoHtml (cargo);
